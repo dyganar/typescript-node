fd1ae93ee01205857e72a9fed9a6bb3065ffcc93
Takanar <dyganar@hotmail.com>
2021-02-19 10:44:58 -0300

    adicionado models e usuarioController

---
+++ src/models/mensagem.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const MensagemSchema = new Schema({
+    texto: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    remetente: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    }
+})
+
+export default model('Mensagem', MensagemSchema)
